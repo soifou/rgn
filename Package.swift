4a4bfe109019dc6884a644cfce5fd69604ae7767
Soifou <fleur.fr@gmail.com>
2026-04-13 23:26:55 +0200

    chore: target recent macos version

--- Package.swift
+++ Package.swift
@@ -1,13 +1,13 @@
 // swift-tools-version: 6.3
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "rgn",
+    platforms: [
+        .macOS(.v13)
+    ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
             name: "rgn"
         ),
